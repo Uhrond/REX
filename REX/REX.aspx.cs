e59828cf5d1a48cac8681aafcbe22574a3f2ed23
Uhrond <s.taillefert@gmail.com>
2016-10-26 21:36:24 +0200

    Add files via upload

---
+++ REX/REX.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+public partial class REX_REX : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       // XElement contacts = new XElement();
+    }
+
+}
---
+++ REX/REX.js
@@ -0,0 +1,15 @@
+﻿/* Constante */
+var Initial = "K";
+var topInitial = 0;
+var leftInitial = 0;
+
+$(document).ready(function () {
+    $("body").append("<div class='REXIcon'>" + Initial + "</div>");
+    $(".REXIcon").css({ 'top': topInitial, 'left': leftInitial });
+    $(".REXIcon").draggable();
+});
+$(document).on('click', '.REXIcon', function (e) {
+    $("body").append("<div class='REXView'></div>");
+    $(".REXView").hide().load("REX/REX.aspx").fadeIn();
+    e.preventDefault();
+});
